4467c83e54e30c2db9dd214a47d192702de3ee14
Beatrice Wambui <bettyneema2@gmail.com>
2025-09-20 22:22:10 +0300

    commits initial property decorator lab

--- lib/dog.py
+++ lib/dog.py
@@ -15,25 +15,28 @@ class Dog:
         self.name = name
         self.breed = breed
 
-    def get_name(self):
+    @property
+    def name(self):
+        """Return the dog's name"""
         return self._name
 
-    def set_name(self, name):
-        if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+    @name.setter
+    def name(self, value):
+        """Validate and set the dog's name"""
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
+            raise ValueError("Name must be string between 1 and 25 characters.")
 
-    name = property(get_name, set_name)
-
-    def get_breed(self):
+    @property
+    def breed(self):
+        """Return the dog's breed"""
         return self._breed
 
-    def set_breed(self, breed):
-        if breed in APPROVED_BREEDS:
-            self._breed = breed
+    @breed.setter
+    def breed(self, value):
+        """Validate and set the dog's breed"""
+        if value in APPROVED_BREEDS:
+            self._breed = value
         else:
             raise ValueError("Breed must be in list of approved breeds.")
-
-    breed = property(get_breed, set_breed)
